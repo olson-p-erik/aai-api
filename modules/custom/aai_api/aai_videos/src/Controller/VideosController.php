80075be2bd72bd3a4c0e9dcc89d87477bfad9aa7
epOlson <erikmuffinman@gmail.com>
2018-03-06 12:00:20 -0600

    Initial creation of aai_api module and aai_videos submodule

---
+++ modules/custom/aai_api/aai_videos/src/Controller/VideosController.php
@@ -0,0 +1,40 @@
+<?php
+
+	namespace Drupal\aai_videos\Controller;
+
+	use Drupal\Core\Controller\ControllerBase;
+
+	class VideosController extends ControllerBase {
+
+		/**
+		 * Callback function to get the data from REST API
+		 */
+		public function getRecentList() {
+
+			$response = null;
+			$result = null;
+			$build = array();
+
+			if ( function_exists( 'aai_api_response' ) ) {
+				$response = aai_api_response( 'https://proofapi.herokuapp.com/videos', 'GET' );
+			}
+
+			if ( $response ) {
+
+				$result = json_decode($response);
+				$data = array();
+
+				$data['title'] = 'Most Recent Videos';
+				$data['videos'] = $result['data'];
+
+				$build = array(
+					//'#theme' => 'videos_recent_list',
+					'#title' => 'Most Recent Videos',
+					'#pagehtml' => 'All the greatest new videos',
+					'#data' => $data
+				);
+			}
+			return $build;
+		}
+	}
+?>
