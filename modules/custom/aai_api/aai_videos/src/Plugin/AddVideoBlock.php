81bf652e2cd666ebd8aed7972059d5a6a143e59d
Erik Olson <olson.p.erik@gmail.com>
2018-03-07 19:57:19 +0000

    Updates to add video block

--- modules/custom/aai_api/aai_videos/src/Plugin/AddVideoBlock.php
+++ modules/custom/aai_api/aai_videos/src/Plugin/AddVideoBlock.php
@@ -5,7 +5,15 @@ namespace Drupal\aai_videos\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Form\FormInterface;
 
-
+/**
+ * Provides the 'Add Video' block.
+ *
+ * @Block(
+ *   id = "add_new_video_block",
+ *   admin_label = @Translation("Add New Video block"),
+ *   category = @Translation("Custom AAI API")
+ * )
+ */
 class AddVideoBlock extends BlockBase {
 	
 	public function build() {
