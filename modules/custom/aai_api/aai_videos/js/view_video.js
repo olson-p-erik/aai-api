da035abd5dde360693e0597423ecd9b90a8d24ae
Erik Olson <olson.p.erik@gmail.com>
2018-03-08 19:45:30 +0000

    Styling view video

---
+++ modules/custom/aai_api/aai_videos/js/view_video.js
@@ -0,0 +1,15 @@
+(function ($) {
+  function voteup() {
+    let votes = document.getElementById('votes');
+    let count = parseInt(votes.innerHTML);
+    count++;
+    votes.innerHTML = count;
+  }
+
+  function votedown() {
+    let votes = document.getElementById('votes');
+    let count = parseInt(votes.innerHTML);
+    count--;
+    votes.innerHTML = count;
+  }
+}(jQuery));
